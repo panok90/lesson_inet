61af72de3c53523cfd8116f528d81ad46aca9b57
panok90 <cool.cyclon@yandex.ru>
2022-02-01 20:06:37 +0300

    created lesson 3

--- lesson-3/main.py
+++ lesson-3/main.py
@@ -3,6 +3,12 @@ from pymongo import MongoClient
 from bs4 import BeautifulSoup
 from pprint import pprint
 
+
+def find_wage(elem, collection):
+    find_list = list(collection.find({'$or': [{'min': {'$gt': elem}}, {'max': {'$gt': elem}}]}))
+    pprint(find_list)
+
+
 job = 'python'
 # Параметры запроса
 params = dict()
@@ -24,7 +30,7 @@ base_list = dom.find_all('div', {'class': 'vacancy-serp-item'})
 result = []
 for item in base_list:
     item_result = dict()
-    wage_list = [None] * 3
+    # wage_list = {'min': None, 'max': None, 'currency': None}
     a_tag = item.find('span', {'class': 'g-user-content'})
     item_result['name'] = a_tag.getText()
     item_result['link'] = list(a_tag.children)[0].get('href')
@@ -33,32 +39,35 @@ for item in base_list:
         str_wage = str(item.find('span', attrs={'class': 'bloko-header-section-3',
                                                 'data-qa': 'vacancy-serp__vacancy-compensation'}).getText())
         params_wage = str_wage.split()
-        wage_list[2] = params_wage[-1]
+        item_result['currency'] = params_wage[-1]
         params_wage = params_wage[:-1]
         if params_wage[0] in let_list:
             int_wage = int(params_wage[1] + params_wage[2])
-            if wage[0] == let_list[0]:
-                wage_list[0] = int_wage
+            if params_wage[0] == let_list[0]:
+                item_result['min'] = int_wage
             else:
-                wage_list[1] = int_wage
+                item_result['max'] = int_wage
         else:
-            int_wage_min = int(wage[0] + wage[1])
-            wage_list[0] = int_wage_min
-            int_wage_max = int(wage[3] + wage[4])
-            wage_list[1] = int_wage_max
-        wage = wage_list
+            int_wage_min = int(params_wage[0] + params_wage[1])
+            item_result['min'] = int_wage_min
+            int_wage_max = int(params_wage[3] + params_wage[4])
+            item_result['max'] = int_wage_max
     except:
-        wage = wage_list
-    item_result['wage'] = wage
+        item_result['currency'] = None
+        item_result['min'] = None
+        item_result['max'] = None
     item_result['site'] = 'hh.ru'
     result.append(item_result)
 
 client = MongoClient('127.0.0.1', 27017)
 db = client['database']
+# db.jobs.drop()
 jobs = db.jobs
 
 for doc in result:
     if jobs.find_one({'link': doc['link']}):
         continue
     jobs.insert_one(doc)
-pprint(len(list(jobs.find({}))))
+
+wage = 200000
+find_wage(wage, jobs)
